45006763e6a9eb781c569ee2ef6ba5401c82161a
robinsh <robinsh@microsoft.com>
2017-11-15 13:41:59 -0800

    Rest Auth API code

---
+++ StorageRestApiAuth/AzureStorageAuthenticationHelper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
+using System.Web;
+using System.Collections;
+
+namespace StorageRestApiAuth
+{
+    /// <summary>
+    /// You can take this class and drop it into another project and use this code
+    /// to create the headers you need to make a REST API call to Azure Storage.
+    /// </summary>
+    internal static class AzureStorageAuthenticationHelper
+    {
+        /// <summary>
+        /// This creates the authorization header. This is required, and must be built 
+        ///   exactly following the instructions. This will return the authorization header
+        ///   for most storage service calls.
+        /// Create a string of the message signature and then encrypt it.
+        /// </summary>
+        /// <param name="storageAccountName">The name of the storage account to use.</param>
+        /// <param name="storageAccountKey">The access key for the storage account to be used.</param>
+        /// <param name="now">Date/Time stamp for now.</param>
+        /// <param name="httpRequestMessage">The HttpWebRequest that needs an auth header.</param>
+        /// <param name="ifMatch">Can someone please give an example of this?</param>
+        /// <param name="md5">Can someone please explain how it's used?</param>
+        /// <returns></returns>
+        internal static AuthenticationHeaderValue GetAuthorizationHeader(
+           string storageAccountName, string storageAccountKey, DateTime now,
+           HttpRequestMessage httpRequestMessage, string ifMatch = "", string md5 = "")
+        {
+            // This is the raw representation of the message signature.
+            HttpMethod method = httpRequestMessage.Method;
+            String MessageSignature = String.Format("{0}\n\n\n{1}\n{5}\n\n\n\n{2}\n\n\n\n{3}{4}",
+                      method.ToString(),
+                      (method == HttpMethod.Get || method == HttpMethod.Head) ? String.Empty
+                        : httpRequestMessage.Content.Headers.ContentLength.ToString(),
+                      ifMatch,
+                      GetCanonicalizedHeaders(httpRequestMessage),
+                      GetCanonicalizedResource(httpRequestMessage.RequestUri, storageAccountName),
+                      md5);
+
+            // Now turn it into a byte array.
+            byte[] SignatureBytes = Encoding.UTF8.GetBytes(MessageSignature);
+
+            // Create the HMACSHA256 version of the storage key.
+            HMACSHA256 SHA256 = new HMACSHA256(Convert.FromBase64String(storageAccountKey));
+
+            // Compute the hash of the SignatureBytes and convert it to a base64 string.
+            string signature = Convert.ToBase64String(SHA256.ComputeHash(SignatureBytes));
+
+            // This is the actual header that will be added to the list of request headers.
+            return new AuthenticationHeaderValue("SharedKey",
+               storageAccountName + ":" + Convert.ToBase64String(SHA256.ComputeHash(SignatureBytes)));
+        }
+
+        /// <summary>
+        /// Put the headers that start with x-ms in a list and sort them.
+        /// Then format them into a string of [key:value\n] values concatenated into one string.
+        /// (Canonicalized Headers = headers where the format is standardized).
+        /// </summary>
+        /// <param name="httpRequestMessage">The request that will be made to the storage service.</param>
+        /// <returns>Error message; blank if okay.</returns>
+        private static string GetCanonicalizedHeaders(HttpRequestMessage httpRequestMessage)
+        {
+            var headers = from kvp in httpRequestMessage.Headers
+                          where kvp.Key.StartsWith("x-ms-", StringComparison.OrdinalIgnoreCase)
+                          orderby kvp.Key
+                          select new { Key = kvp.Key.ToLowerInvariant(), kvp.Value };
+
+            StringBuilder sb = new StringBuilder();
+
+            // Create the string in the right format; this is what makes the headers "canonicalized" --
+            //   it means put in a standard format. http://en.wikipedia.org/wiki/Canonicalization
+            foreach (var kvp in headers)
+            {
+                StringBuilder headerBuilder = new StringBuilder(kvp.Key);
+                char separator = ':';
+
+                // Get the value for each header, strip out \r\n if found, then append it with the key.
+                foreach (string headerValues in kvp.Value)
+                {
+                    string trimmedValue = headerValues.TrimStart().Replace("\r\n", String.Empty);
+                    headerBuilder.Append(separator).Append(trimmedValue);
+
+                    // Set this to a comma; this will only be used 
+                    //   if there are multiple values for one of the headers.
+                    separator = ',';
+                }
+                sb.Append(headerBuilder.ToString()).Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// This part of the signature string represents the storage account 
+        ///   targeted by the request. Will also include any additional query parameters/values.
+        /// For ListContainers, this will return something like this:
+        ///   /storageaccountname/\ncomp:list
+        /// </summary>
+        /// <param name="address">The URI of the storage service.</param>
+        /// <param name="accountName">The storage account name.</param>
+        /// <returns></returns>
+        private static string GetCanonicalizedResource(Uri address, string storageAccountName)
+        {
+            // The absolute path is "/" because for we're getting it a list of containers.
+            StringBuilder sb = new StringBuilder("/").Append(storageAccountName).Append(address.AbsolutePath);
+            NameValueCollection values2 = new NameValueCollection();
+
+            // Address.Query is the resource, such as "?comp=list".
+            // This ends up with a NameValueCollection with 1 entry having key=comp, value=list.
+            // It will have more entries if you have more query parameters.
+            NameValueCollection values = HttpUtility.ParseQueryString(address.Query);
+            foreach (string str2 in values.Keys)
+            {
+                ArrayList list = new ArrayList(values.GetValues(str2));
+                list.Sort();
+                StringBuilder builder2 = new StringBuilder();
+                foreach (object obj2 in list)
+                {
+                    if (builder2.Length > 0)
+                    {
+                        builder2.Append(",");
+                    }
+                    builder2.Append(obj2.ToString());
+                }
+                values2.Add((str2 == null) ? str2 : str2.ToLowerInvariant(), builder2.ToString());
+            }
+            ArrayList list2 = new ArrayList(values2.AllKeys);
+            list2.Sort();
+            foreach (string str3 in list2)
+            {
+                StringBuilder builder3 = new StringBuilder(string.Empty);
+                builder3.Append(str3).Append(":").Append(values2[str3]).Append("\n");
+                sb.Append(builder3.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
